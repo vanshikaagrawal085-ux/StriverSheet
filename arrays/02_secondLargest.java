3ee695df3fa3c96a70be04127de0d699bcb46a5f
vanshikaagrawal085-ux <vanshikaagrawal085@gmail.com>
2026-02-22 15:40:33 +0530

    Create 02_secondLargest.java

---
+++ arrays/02_secondLargest.java
@@ -0,0 +1,16 @@
+lass Solution {
+    public int secondLargestElement(int[] nums) {
+    int n = nums.length;
+    int largest = 0;
+    int Slargest = 0;
+    for(int i=0; i<n; i++){
+        if(largest<nums[i]){
+            Slargest = largest;
+            largest = nums[i];
+        }
+        else if(nums[i]>Slargest && nums[i]<largest){
+            Slargest = nums[i];
+        }
+    } return (Slargest == 0) ? -1 : Slargest ;
+}
+}
